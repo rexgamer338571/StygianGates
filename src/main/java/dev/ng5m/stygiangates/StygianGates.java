e1e68046a40a8ee1ffeda7a371a9de488ed5abe9
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-05-25 13:28:46 +0200

    initial commit

--- src/main/java/dev/ng5m/stygiangates/StygianGates.java
+++ src/main/java/dev/ng5m/stygiangates/StygianGates.java
@@ -3,15 +3,21 @@ package dev.ng5m.stygiangates;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StygianGates extends JavaPlugin {
+    public static String NEWEST_VER = "1.0.0";
+    private static StygianGates instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static StygianGates getInstance() {
+        return instance;
     }
 }
---
+++ src/main/java/dev/ng5m/stygiangates/util/Updater.java
@@ -0,0 +1,50 @@
+package dev.ng5m.stygiangates.util;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import dev.ng5m.stygiangates.StygianGates;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Updater {
+
+    public static void update() throws IOException {
+        HttpURLConnection connCheck =  (HttpURLConnection) new URL("https://api.github.com/repos/rexgamer338571/StygianGates/releases/latest").openConnection();
+
+        String newestVer;
+
+        try (InputStream is = connCheck.getInputStream(); Scanner scanner = new Scanner(is)) {
+            String response = scanner.useDelimiter("\\A").next();
+
+            JsonObject releaseInfo = JsonParser.parseString(response).getAsJsonObject();
+            newestVer = releaseInfo.get("tag_name").getAsString();
+        }
+
+        if (!newestVer.equals(StygianGates.NEWEST_VER)) {
+            Bukkit.broadcast(Component.text("No new version of StygianGates is available.").color(TextColor.color(0, 255, 0)), "minecraft.command.op");
+            return;
+        }
+
+        String updateURL = "https://github.com/rexgamer338571/StygianGates/releases/download/" + newestVer + "/sg-" + newestVer + ".jar";
+
+        try (BufferedInputStream is = new BufferedInputStream(new URL(updateURL).openStream())) {
+            FileOutputStream fos = new FileOutputStream(StygianGates.getInstance().getDataFolder() + File.separator + "plugins" + File.separator + "sg-" + newestVer + ".jar");
+            final byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = is.read(dataBuffer, 0, 1024)) != -1) {
+                fos.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException x) {
+            x.printStackTrace();
+        }
+
+    }
+
+}
