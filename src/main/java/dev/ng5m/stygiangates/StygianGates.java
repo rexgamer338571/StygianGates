0c4f046b6acf23393a9d1ea42d49ceffa58b89b4
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-05-25 12:45:13 +0200

    initial commit

---
+++ src/main/java/dev/ng5m/stygiangates/StygianGates.java
@@ -0,0 +1,17 @@
+package dev.ng5m.stygiangates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StygianGates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
